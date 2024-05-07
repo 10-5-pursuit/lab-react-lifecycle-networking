84947b896df831ccdefffacb86e3d22a65ca5a53
eivor9 <njeremiah@pursuit.org>
2024-05-07 15:41:41 -0400

    complete lab

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,13 +1,36 @@
 import PetList from "./PetList";
 import "./Employee.css";
+import { useState, useEffect } from "react";
 
-export const Employee = () => {
+export const Employee = ({employee}) => {
+  const {id, firstName, lastName, prefix, postfix, title} = employee;
+  const [show, changeShowing] = useState(false);
+  const [pets, setPets] = useState([]);
+
+  function switchShowing(){
+    changeShowing(!show);
+  }
+  function getPets(){
+    fetch("https://vet-app-0obi.onrender.com/api/pets")
+      .then((response) => response.json())
+      .then((json) => {
+        setPets(json);
+      })
+      .catch((err) => {
+        console.log("error fetching list of employees");
+      });
+  }
+
+  useEffect(() => {
+    getPets();
+  }, [])
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      <h3>{prefix} {firstName} {lastName} {postfix ? `, ${postfix}`: ""}</h3>
+      <h4>{title}</h4>
+      <button onClick={switchShowing}>Show Pets</button>
+      {show ? <PetList pets={pets} id={id}/> : ""}
     </article>
   );
 };
--- src/Components/PetList.jsx
+++ src/Components/PetList.jsx
@@ -1,7 +1,15 @@
-export const PetList = () => {
+export const PetList = ({pets, id}) => {
+
+  pets = pets.filter(pet => pet.employeeId === id);
+  pets = pets.map(pet => pet.name);
+  pets = pets.join(", ");
+
   return (
     <aside className="pets-list">
-      <p>No pets listed for this employee.</p>
+      {pets ? <p>{pets}</p> : 
+        <p>No pets listed for this employee.</p>
+      }
+      
     </aside>
   );
 };
