0d0f249bb4849cd3d5e4d3a682c946dd307bbdf5
kbodur <kbodur@pursuit.org>
2024-05-17 03:49:59 -0400

    completed lab

--- src/Components/Employee.jsx
+++ src/Components/Employee.jsx
@@ -1,13 +1,55 @@
+
+import React, { useState, useEffect } from "react";
 import PetList from "./PetList";
 import "./Employee.css";
 
-export const Employee = () => {
+const API = "https://vet-app-0obi.onrender.com/api/employees";
+
+const Employee = () => {
+  const [employees, setEmployees] = useState([]);
+  const [selectedEmployeeId, setSelectedEmployeeId] = useState(null);
+  const [petList, setPetList] = useState([]);
+
+  useEffect(() => {
+    fetch(`${API}`)
+      .then((res) => res.json())
+      .then((data) => {
+        setEmployees(data);
+      })
+      .catch((error) => {
+        console.error("Error fetching employees:", error);
+      });
+  }, []);
+
+  const handleShowPets = (employeeId) => {
+    if (employeeId === selectedEmployeeId) {
+      setSelectedEmployeeId(null);
+      setPetList([]);
+    } else {
+      fetch(`${API}/pets?employeeId=${employeeId}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setSelectedEmployeeId(employeeId);
+          setPetList(data);
+        })
+        .catch((error) => {
+          console.error("Error fetching pets:", error);
+        });
+    }
+  };
+
   return (
     <article className="employee">
-      <h3>Staff Member Name</h3>
-      <h4>Staff Member Title</h4>
-      <button>Show Pets</button>
-      <PetList />
+      {employees.map((employee) => (
+        <div className="emp" key={employee.id}>
+          <h3>{`${employee.prefix || ""} ${employee.firstName} ${employee.lastName}${employee.postfix ? `, ${employee.postfix}` : ""}`}</h3>
+          <h4>{employee.title}</h4>
+          <button onClick={() => handleShowPets(employee.id)}>
+            {selectedEmployeeId === employee.id ? "Hide Pets" : "Show Pets"}
+          </button>
+          {selectedEmployeeId === employee.id && <PetList employeeId={employee.id} />}
+        </div>
+      ))}
     </article>
   );
 };
